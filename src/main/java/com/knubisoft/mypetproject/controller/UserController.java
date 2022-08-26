fe781eead8e3b4b6b6de59280c5a9558edb69d84
Oksana <marjash@i.ua>
2022-08-26 22:17:29 +0300

    added create user

--- src/main/java/com/knubisoft/mypetproject/controller/UserController.java
+++ src/main/java/com/knubisoft/mypetproject/controller/UserController.java
@@ -7,6 +7,7 @@ import com.knubisoft.mypetproject.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,21 +17,24 @@ public class UserController {
 
     @Autowired
     UserServiceImpl userService;
+
+    @Autowired
     CityServiceImpl cityService;
 
 
-    @GetMapping("/reg/form")
+    @GetMapping("/user/create")
     public String regForm(Model model) {
         model.addAttribute("user", new User());
         model.addAttribute("cities", cityService.getAll());
         return "reg_form";
     }
 
-    @PostMapping("/reg/form")
-    public String regFormSubmit(@ModelAttribute User user, Model model){
-        model.addAttribute("user", user);
-        return "result";
+    @PostMapping("/user/create")
+    public String regFormSubmit(@Validated @ModelAttribute("user") User user){
+        userService.save(user);
+        return "redirect:/user/all";
     }
+
 
 
     @GetMapping("user/all")
--- src/main/java/com/knubisoft/mypetproject/model/User.java
+++ src/main/java/com/knubisoft/mypetproject/model/User.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private int id;
     private String name;
     private String surname;
     private String email;
