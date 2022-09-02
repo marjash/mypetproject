766de2dd8362ff9ecfb2bcae352d13b18804ed66
Oksana <marjash@i.ua>
2022-09-02 13:23:10 +0300

    add login from facebook and get user

--- src/main/java/com/knubisoft/mypetproject/controller/UserController.java
+++ src/main/java/com/knubisoft/mypetproject/controller/UserController.java
@@ -1,25 +1,17 @@
 package com.knubisoft.mypetproject.controller;
 
-import com.knubisoft.mypetproject.model.Advert;
 import com.knubisoft.mypetproject.model.User;
 import com.knubisoft.mypetproject.repository.UserRepository;
-import com.knubisoft.mypetproject.service.CityServiceImpl;
 import com.knubisoft.mypetproject.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 //@RequestMapping("/")
@@ -38,9 +30,13 @@ public class UserController {
         User user = null;
         Authentication authentication =
                 SecurityContextHolder.getContext().getAuthentication();
-        if (authentication.getClass().getName().equals("org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken")) {
-            DefaultOidcUser defaultOidcUser = (DefaultOidcUser) authentication.getPrincipal();
-            String email = defaultOidcUser.getEmail();
+        if (((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId().equals("google")){
+            DefaultOidcUser google = (DefaultOidcUser) authentication.getPrincipal();
+            String email = google.getEmail();
+            user = userRepository.findByEmail(email);
+        }
+        else if (((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId().equals("facebook")){
+            String email = (String) (((OAuth2AuthenticationToken) authentication).getPrincipal()).getAttributes().get("email");
             user = userRepository.findByEmail(email);
         }
         else {
